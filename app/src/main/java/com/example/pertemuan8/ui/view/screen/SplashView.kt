d6c4a3147b8a5f39bb38707eb2305e108e758068
moh. nurwahid <mohnurwaid923@gmail.com>
2024-11-22 16:07:52 +0700

    memperbaiki splash view

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/SplashView.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,9 +37,17 @@ fun SplashView(
         Image(
             painter = painterResource(id = R.drawable.img),
             contentDescription = "",
-            modifier = Modifier.size(150.dp)
+            modifier = Modifier.size(16.dp)
         )
-        
-
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = {onMulaiButton()
+            },
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp)
+        ) {
+            Text(text = "Mulai")
+        }
     }
 }
